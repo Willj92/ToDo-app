e8aec8a9e70ea42559067c9d2eca2c066eb468db
gabrielcyrino <gfc93@hotmail.com>
2015-06-20 19:51:34 -0300

    configurado istanbul e inicio dos testes

--- test/todoapp.js
+++ test/todoapp.js
@@ -1,1 +1,10 @@
+var assert = require("assert");
 
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('deve retornar -1 quando o valor não está presente', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
