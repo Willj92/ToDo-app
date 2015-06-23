16282d103065fc83fb04cb3b50d5fbd8788e6100
gabrielcyrino <gfc93@hotmail.com>
2015-06-23 08:58:37 -0300

    alguns testes inúteis

--- test/todoapp.js
+++ test/todoapp.js
@@ -3,13 +3,46 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'test';
 var should = require("should");
 var math = require('../util/matematica');
 
-describe('Array', function() {
+describe('Math', function() {
     describe('Math, somar', function() {
         it('deve retornar 3 quando o valor passado for 1 e 2', function() {
             (math.somar(1, 2)).should.be.equal(3);
         });
         it('deve retornar -1 quando o valor passado for 1 e -2', function() {
             (math.somar(1, -2)).should.be.equal(-1);
+        });
+        it('deve retornar 0 quando o valor passado for 0 e 0', function() {
+            (math.somar(0, 0)).should.be.equal(0);
+        });
+    });
+    describe('Math, subtrair', function() {
+        it('deve retornar 1 quando o valor passado for 2 e 1', function() {
+            (math.somar(2, 1)).should.be.equal(1);
+        });
+        it('deve retornar -1 quando o valor passado for 1 e 2', function() {
+            (math.somar(1, 2)).should.be.equal(-1);
+        });
+        it('deve retornar 0 quando o valor passado for 0 e 0', function() {
+            (math.somar(0, 0)).should.be.equal(0);
+        });
+    });
+    describe('Math, multiplicar', function() {
+        it('deve retornar 2 quando o valor passado for 2 e 1', function() {
+            (math.somar(2, 1)).should.be.equal(2);
+        });
+        it('deve retornar -2 quando o valor passado for -1 e 2', function() {
+            (math.somar(-1, 2)).should.be.equal(-2);
+        });
+        it('deve retornar 0 quando o valor passado for 0 e 0', function() {
+            (math.somar(0, 0)).should.be.equal(0);
+        });
+    });
+    describe('Math, dividir', function() {
+        it('deve retornar 2 quando o valor passado for 2 e 1', function() {
+            (math.somar(2, 1)).should.be.equal(2);
+        });
+        it('deve retornar -1 quando o valor passado for 1 e 2', function() {
+            (math.somar(-4, 2)).should.be.equal(-2);
         });
         it('deve retornar 0 quando o valor passado for 0 e 0', function() {
             (math.somar(0, 0)).should.be.equal(0);
--- util/matematica.js
+++ util/matematica.js
@@ -1,3 +1,15 @@
 module.exports.somar = function(num1, num2) {
     return num1 + num2;
 };
+
+module.exports.subtrair = function(num1, num2) {
+    return num1 - num2;
+};
+
+module.exports.multiplicar = function(num1, num2) {
+    return num1 * num2;
+};
+
+module.exports.dividir = function(num1, num2) {
+    return num1 / num2;
+};
